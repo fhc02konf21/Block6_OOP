f4647661ae793442c1a8878d84b9bb6f99942cde
skochauf <stephan.kochauf@lv.campus02.at>
2021-11-26 17:08:35 +0100

    DemoDogApp, Beagle

--- src/org/campus02/animal/Beagle.java
+++ src/org/campus02/animal/Beagle.java
@@ -1,4 +1,15 @@
 package org.campus02.animal;
 
+/* Schlüsselwort extends => Damit wird die Vererbung eingeleitet */
 public class Beagle extends Dog{
+    public String lovedFood;
+
+    @Override
+    public String toString() {
+        return "Beagle{" +
+                "lovedFood='" + lovedFood + '\'' +
+                ", eyeColor='" + eyeColor + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
--- src/org/campus02/animal/DemoDogApp.java
+++ src/org/campus02/animal/DemoDogApp.java
@@ -3,5 +3,20 @@ package org.campus02.animal;
 public class DemoDogApp {
     public static void main(String[] args) {
 
+        Dog d = new Dog();
+        d.eyeColor = "brown";
+        d.weight = 2900;
+
+        System.out.println(d.toString());
+        d.doSomething();
+
+        Beagle b = new Beagle();
+        b.eyeColor = "blue";
+        b.weight = 2850;
+        b.lovedFood = "burger";
+
+        b.doSomething();
+        System.out.println(b.toString());
+
     }
 }
